2919305656fd6e7da3c9a9a852914dc852e76649
Narmin Alieva <narminaliev@gmail.com>
2018-02-15 02:56:16 +0200

    added boilerplate for shop and orders components, set up routing

--- nfq-proj/src/app/app.module.ts
+++ nfq-proj/src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ShopComponent } from './shop.component';
+import { OrdersComponent } from './orders.component';
 
+const APP_ROUTES: Routes = [
+  { path: '', component: ShopComponent },
+  { path: 'orders', component: OrdersComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent,
+    OrdersComponent
   ],
   imports: [
+    RouterModule.forRoot(APP_ROUTES),
     BrowserModule
   ],
   providers: [],
